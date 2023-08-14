d5fb08780999d64a7cee030b4d400eb2b88c58c9
bodiabeliy <bboda2106@gmail.com>
2023-08-14 14:15:42 +0300

    plug in React to project; adding config webpack for css files

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,7 @@
 import {render} from "react-dom";
-import App from "./app/App";
-import {BrowserRouter} from "react-router-dom";
-import {ThemeProvider} from "app/providers/ThemeProvider";
-
-import "./shared/config/i18n/i18n";
+import App from "./App";
 
 render(
-    <BrowserRouter>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
-    </BrowserRouter>,
+    <App />,
     document.getElementById('root')
 )
